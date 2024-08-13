6bd77bb231770914ef9c9c1070aff8a606c0a45e
VaggelisAn <52158233+VaggelisAn@users.noreply.github.com>
2024-08-13 23:20:51 +0300

    Update sensors.py

--- sensors.py
+++ sensors.py
@@ -1,32 +1,88 @@
-# SENSORS
-# sensors.py
-
-# ---------------------
-# - - - Local Files - -
-import config
-
-
-# - - - Libraries - - -
-import numpy as np
+import datetime as dt
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import board
+import adafruit_shtc3
 
-# ---------------------
+# - - BOARD - -
+i2c = board.I2C()   # uses board.SCL and board.SDA
+    
+# - - SENSORS - -    
+sht = adafruit_shtc3.SHTC3(i2c)
 
-from machine import I2C, Pin, SoftI2C # Micropython
-from time import sleep
+def read_shtc3():
+    global sht
+    try:
+        temperature, relative_humidity = sht.measurements
+        print("Temperature: %0.1f C" % temperature)
+        print("Humidity: %0.1f %%" % relative_humidity)
+        print("")
+    except Exception as error:
+        print(f"Error: {error}")
+        temperature, relative_humidity = (-1, -1)
+    return (temperature, relative_humidity)
 
-# source: https://www.reddit.com/r/MicroPythonDev/comments/1d7cpyr/shtc3_sensor/
-def read_shtc():
-    i2c=SoftI2C(scl=Pin(17),sda=Pin(16))
-    buf1=bytearray([0x7C,0xA2])
-    while True:
-        i2c.writeto(0x70,buf1)
-        buf2=bytearray([0x00,0x00,0x00,0x00,0x00,0x00])
-        i2c.readfrom_into(0x70,buf2)
-        tempC=(buf2[1]|(buf2[0]<<8))*175/65536-45
-        tempF=(tempC*1.8)+32
-        humid=(buf2[4]|(buf2[3]<<8))*100/65536
-        print('Temperature:',tempC,'C')
-        print('Temperature:',tempF,'F')
-        print('Humidity:',humid,'%')
-        sleep(5)
+
+# Create figure for plotting
+fig, (ax1, ax2) = plt.subplots(2, 1)
+xs = []
+ys_temp = []
+ys_humidity = []
+
+time_counter = 0
+
+line1, = ax1.plot([], [], lw=2, label='Air Temperature')
+line2, = ax2.plot([], [], lw=2, label='Air Humidity')
+
+ax1.set_xlim(0, 600)
+ax1.set_ylim(10, 40)
+
+ax2.set_xlim(0, 600)
+ax2.set_ylim(0, 100)
+
+ax1.set_title("Air Temperature")
+ax2.set_title("Air Humidity")
+ax1.set_ylabel("Air Temperature (C)")
+ax2.set_ylabel("Air Humidity (%RH)")
+ax1.set_xlabel("Time (s)")
+ax2.set_xlabel("Time (s)")
+
+
+# This function is called periodically from FuncAnimation
+def animate(frame):
+    global time_counter
+
+    # Read temperature (Celsius) from TMP102
+    temp, humidity = read_shtc3()
+
+    # Add x and y to lists
+    xs.append(time_counter)
+    time_counter += 1
+    ys_temp.append(temp)
+    ys_humidity.append(humidity)
+
+    line1.set_data(xs, ys_temp)
+    line2.set_data(xs, ys_humidity)
+    
+    if time_counter >= ax1.get_xlim()[1]:
+        ax1.set_xlim(ax1.get_xlim()[0], time_counter + 60)
+        ax2.set_xlim(ax2.get_xlim()[0], time_counter + 60)
+    
+    return line1, line2
+    
+    # Draw x and y lists
+    ax.clear()
+    plt.subplot(1, 1, 1)
+    ax.plot(xs, ys_temp)
+    plt.subplot(2, 1, 1)
+    ax.plot(xs, ys_humidity)
+
+    # Format plot
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('Temperature and Humidity over Time')
+    plt.ylabel('Temperature (deg C)')
+
+# Set up plot to call animate() function periodically
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
